32867b61acd50354b7d4a1453ce5684b08174deb
Bohica <ab3.cisco@gmail.com>
2024-09-21 14:20:51 -0700

    Update Datapath.cs

--- Scripts/Misc/DataPath.cs
+++ Scripts/Misc/DataPath.cs
@@ -8,14 +8,14 @@ namespace Server.Misc
 	public class DataPath
 	{
 		/* If you have not installed Ultima Online,
-		 * or wish the server to use a separate set of datafiles,
+		 * or wish the server to use a separate set of data files,
 		 * change the 'CustomPath' value.
 		 * Example:
 		 *  private static string CustomPath = @"C:\Program Files\Ultima Online";
 		 */
-		private static string CustomPath = null;
+		private static string CustomPath = @"C:\Users\admin\Desktop\UOClients\T2A_5.0.8.3"; //change this to the path of your UO client folder
 
-		/* The following is a list of files which a required for proper execution:
+		/* The following is a list of files which are required for proper execution:
 		 * 
 		 * Multi.idx
 		 * Multi.mul
@@ -33,66 +33,66 @@ namespace Server.Misc
 
 		public static void Configure()
 		{
-			string pathUO = GetPath( @"Origin Worlds Online\Ultima Online\1.0", "ExePath" );
-			string pathTD = GetPath( @"Origin Worlds Online\Ultima Online Third Dawn\1.0", "ExePath" ); //These refer to 2D & 3D, not the Third Dawn expansion
-			string pathKR = GetPath( @"Origin Worlds Online\Ultima Online\KR Legacy Beta", "ExePath" ); //After KR, This is the new registry key for the 2D client
-			string pathSA = GetPath( @"Electronic Arts\EA Games\Ultima Online Stygian Abyss Classic", "InstallDir" );
-			string pathHS = GetPath( @"Electronic Arts\EA Games\Ultima Online Classic", "InstallDir" );
+			string pathUO = GetPath(@"Origin Worlds Online\Ultima Online\1.0", "ExePath");
+			string pathTD = GetPath(@"Origin Worlds Online\Ultima Online Third Dawn\1.0", "ExePath"); //These refer to 2D & 3D, not the Third Dawn expansion
+			string pathKR = GetPath(@"Origin Worlds Online\Ultima Online\KR Legacy Beta", "ExePath"); //After KR, This is the new registry key for the 2D client
+			string pathSA = GetPath(@"Electronic Arts\EA Games\Ultima Online Stygian Abyss Classic", "InstallDir");
+			string pathHS = GetPath(@"Electronic Arts\EA Games\Ultima Online Classic", "InstallDir");
 
-			if ( CustomPath != null ) 
-				Core.DataDirectories.Add( CustomPath ); 
+			if (CustomPath != null)
+				Core.DataDirectories.Add(CustomPath);
 
-			if ( pathUO != null ) 
-				Core.DataDirectories.Add( pathUO ); 
+			if (pathUO != null)
+				Core.DataDirectories.Add(pathUO);
 
-			if ( pathTD != null ) 
-				Core.DataDirectories.Add( pathTD );
+			if (pathTD != null)
+				Core.DataDirectories.Add(pathTD);
 
-			if ( pathKR != null )
-				Core.DataDirectories.Add( pathKR );
+			if (pathKR != null)
+				Core.DataDirectories.Add(pathKR);
 
-			if ( pathSA != null )
-				Core.DataDirectories.Add( pathSA );
+			if (pathSA != null)
+				Core.DataDirectories.Add(pathSA);
 
-			if ( pathHS != null )
-				Core.DataDirectories.Add( pathHS );
+			if (pathHS != null)
+				Core.DataDirectories.Add(pathHS);
 
-			if ( Core.DataDirectories.Count == 0 && !Core.Service )
+			if (Core.DataDirectories.Count == 0 && !Core.Service)
 			{
-				Console.WriteLine( "Enter the Ultima Online directory:" );
-				Console.Write( "> " );
+				Console.WriteLine("Enter the Ultima Online directory:");
+				Console.Write("> ");
 
-				Core.DataDirectories.Add( Console.ReadLine() );
+				Core.DataDirectories.Add(Console.ReadLine());
 			}
 		}
 
-		private static string GetPath( string subName, string keyName )
+		private static string GetPath(string subName, string keyName)
 		{
 			try
 			{
 				string keyString;
 
-				if( Core.Is64Bit )
+				if (Core.Is64Bit)
 					keyString = @"SOFTWARE\Wow6432Node\{0}";
 				else
 					keyString = @"SOFTWARE\{0}";
 
-				using( RegistryKey key = Registry.LocalMachine.OpenSubKey( String.Format( keyString, subName ) ) )
+				using (RegistryKey key = Registry.LocalMachine.OpenSubKey(String.Format(keyString, subName)))
 				{
-					if( key == null )
+					if (key == null)
 						return null;
 
-					string v = key.GetValue( keyName ) as string;
+					string v = key.GetValue(keyName) as string;
 
-					if( String.IsNullOrEmpty( v ) )
+					if (String.IsNullOrEmpty(v))
 						return null;
 
-					if ( keyName == "InstallDir" )
+					if (keyName == "InstallDir")
 						v = v + @"\";
 
-					v = Path.GetDirectoryName( v );
+					v = Path.GetDirectoryName(v);
 
-					if ( String.IsNullOrEmpty( v ) )
+					if (String.IsNullOrEmpty(v))
 						return null;
 
 					return v;
